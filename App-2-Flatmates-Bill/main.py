825eacee2941aed2e61817b3611785f1079b15d2
Marcin Walkowski <marcin.walkowski@partner.bmw.de>
2024-06-09 16:03:53 +0200

    Some improvements

--- App-2-Flatmates-Bill/main.py
+++ App-2-Flatmates-Bill/main.py
@@ -3,14 +3,15 @@ class Bill:
         self.amount = amount
         self.period = period
 
-
 class Flatmate:
     def __init__(self, name, days_in_house):
         self.name = name
         self.days_in_house = days_in_house
 
-    def pay(self, bill):
-        return bill.amount / 2
+    def pays(self, bill, mate):
+        weight = self.days_in_house / (self.days_in_house + mate.days_in_house)
+        to_pay = bill.amount * weight
+        return  to_pay
 
 class PdfReport:
     def __init__(self, filename):
@@ -19,10 +20,9 @@ class PdfReport:
     def generate(self, flatmate1, flatmate2, bill):
         pass
 
-
 my_bill = Bill(amount=150, period="June 2024")
-marc = Flatmate(name = "Marcel", days_in_house=15)
-ola = Flatmate(name="Aleksandra", days_in_house=19)
-
-print(marc.pay(bill=my_bill))
-print(ola.pay(bill=my_bill))
+marc = Flatmate(name = "Marcel", days_in_house=9)
+ola = Flatmate(name="Aleksandra", days_in_house=10)
+print("Marcel pays: ", marc.pays(bill=my_bill, mate=ola))
+print("Aleksandra pays: ", ola.pays(bill=my_bill, mate=marc))
+#print(ola.pay(bill=my_bill))
